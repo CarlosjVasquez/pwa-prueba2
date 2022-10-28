4441e5b8798cdd1bd85225b22989b18ee35c563d
Francisco <francisco.ulloa@nextisolutions.com>
2022-10-28 16:57:47 -0500

    [ADD] docker conf

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
-const withPWA = require('next-pwa')({
-  dest: 'public',
+const withPWA = require("next-pwa")({
+  dest: "public",
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === "development"
-})
+  disable: process.env.NODE_ENV === "development",
+});
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -11,7 +11,8 @@ const nextConfig = {
   swcMinify: true,
   compiler: {
     styledComponents: true,
-  }
-}
+  },
+  output: "standalone",
+};
 
 module.exports = withPWA(nextConfig);
