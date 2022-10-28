f219655a9373280d764be3a1acd6fe9cc3251154
Jhon Andrade <jhon.cone.8@gmail.com>
2022-10-27 19:40:03 -0500

    [ADD] Prueba de concepto

--- components/Navbar/Navbar.styles.ts
+++ components/Navbar/Navbar.styles.ts
@@ -1,6 +1,8 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 import BSNavbar from "react-bootstrap/Navbar";
 
 export const Navbar = styled(BSNavbar)`
-  background-color: #F7F7F7
+  background-color: #f7f7f7;
+  height: 90px;
+  width: "100%";
 `;
--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -2,7 +2,7 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import BSNavbar from "react-bootstrap/Navbar";
 import Image from "next/image";
-import * as S from './Navbar.styles';
+import * as S from "./Navbar.styles";
 
 const Navbar: React.FunctionComponent = () => {
   return (
@@ -10,29 +10,87 @@ const Navbar: React.FunctionComponent = () => {
       <Container>
         <BSNavbar.Brand href="#home">
           <Image
-            src="/icon-512x512.png"
-            width="30"
-            height="30"
+            src="/difierelo-512x512.png"
+            width="120"
+            height="40"
             className="d-inline-block align-top"
             alt="React Bootstrap logo"
           />
         </BSNavbar.Brand>
         <BSNavbar.Toggle aria-controls="navbarScroll" />
-        <BSNavbar.Collapse id="navbarScroll">
+        <BSNavbar.Collapse
+          id="navbarScroll"
+          style={{
+            backgroundColor: "#f7f7f7",
+            paddingBottom: 10,
+            paddingInlineStart: 10,
+          }}
+        >
           <Nav
             className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: "100px" }}
+            style={{ maxHeight: "550px" }}
             navbarScroll
           >
-            <Nav.Link href="#action1">Inicio</Nav.Link>
-            <Nav.Link href="#action2">¿Cómo funciona?</Nav.Link>
-            <Nav.Link href="#">Negocios</Nav.Link>
-            <Nav.Link href="#">Contactos</Nav.Link>
+            <Nav.Link
+              href="#action1"
+              style={{ color: "black", fontSize: 14, fontWeight: "bold" }}
+            >
+              Inicio
+            </Nav.Link>
+            <Nav.Link
+              href="#action2"
+              style={{ color: "black", fontSize: 14, fontWeight: "bold" }}
+            >
+              ¿Cómo funciona?
+            </Nav.Link>
+            <Nav.Link
+              href="#"
+              style={{ color: "black", fontSize: 14, fontWeight: "bold" }}
+            >
+              Negocios
+            </Nav.Link>
+            <Nav.Link
+              href="#"
+              style={{ color: "black", fontSize: 14, fontWeight: "bold" }}
+            >
+              Contactos
+            </Nav.Link>
           </Nav>
+          <button
+            className="btn btn-w-m btn-success btn-sm"
+            style={{
+              borderRadius: 10,
+              backgroundColor: "#4237AA",
+              borderColor: "#4237AA",
+              height: 45,
+              width: 130,
+              fontSize: 14,
+              fontWeight: "bold",
+            }}
+          >
+            Iniciar Sesión
+          </button>
+          <button
+            className="btn btn-w-m btn-light btn-sm"
+            style={{
+              marginLeft: 10,
+              borderRadius: 10,
+              borderColor: "#4237AA",
+              height: 45,
+              width: 130,
+              fontSize: 14,
+              fontWeight: "bold",
+              color: "#4237AA",
+              borderStyle: "solid",
+              borderWidth: 2,
+            }}
+          >
+            Regístrate
+          </button>
         </BSNavbar.Collapse>
       </Container>
     </S.Navbar>
   );
-}
+};
 
 export default Navbar;
