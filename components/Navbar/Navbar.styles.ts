c95e3a3449ec1fef088e66ab2eba084f5eb54158
Jhon Andrade <jhon.cone.8@gmail.com>
2022-10-28 20:33:18 -0500

    [ADD, FIX] Icons svg, estilos en navbar

--- components/Navbar/Navbar.styles.ts
+++ components/Navbar/Navbar.styles.ts
@@ -4,5 +4,65 @@ import BSNavbar from "react-bootstrap/Navbar";
 export const Navbar = styled(BSNavbar)`
   background-color: #f7f7f7;
   height: 90px;
-  width: "100%";
+  width: 100%;
+
+  h6 {
+    color: #11171d;
+    font-size: 14px;
+    font-weight: 600;
+    font-family: Eina 02;
+    padding-top: 3.5vh;
+    font-style: normal;
+    line-height: 19px;
+  }
+`;
+
+export const Collapse = styled(BSNavbar.Collapse)`
+  background-color: #f7f7f7;
+  padding-bottom: 10px;
+  padding-inline-start: 10px;
+`;
+
+export const BtnSuccess = styled.button`
+  margin-inline-end: 10px;
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  padding: 16px 24px;
+  gap: 8px;
+  width: 150px;
+  height: 55px;
+  background: #4544ed;
+  border-radius: 8px;
+  font-family: Eina 02;
+  font-style: normal;
+  font-weight: 700;
+  font-size: 16px;
+  line-height: 22px;
+  text-align: center;
+  letter-spacing: -0.02em;
+  color: #ffffff;
+  float: left;
+`;
+
+export const BtnLight = styled.button`
+  margin-inline-start: 10px;
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  padding: 16px 40px;
+  gap: 8px;
+  width: 150px;
+  height: 55px;
+  background: #f8f8f4;
+  border-radius: 8px;
+  font-family: Eina 02;
+  font-style: normal;
+  font-weight: 700;
+  font-size: 16px;
+  line-height: 22px;
+  text-align: center;
+  letter-spacing: -0.02em;
+  color: #4544ed;
+  border-color: #4544ed;
 `;
--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -10,84 +10,40 @@ const Navbar: React.FunctionComponent = () => {
       <Container>
         <BSNavbar.Brand href="#home">
           <Image
-            src="/difierelo-512x512.png"
-            width="120"
+            src="/icons/difierelo-512x512.svg"
+            width="150"
             height="40"
             className="d-inline-block align-top"
-            alt="React Bootstrap logo"
+            alt="Difierelo"
           />
         </BSNavbar.Brand>
         <BSNavbar.Toggle aria-controls="navbarScroll" />
-        <BSNavbar.Collapse
-          id="navbarScroll"
-          style={{
-            backgroundColor: "#f7f7f7",
-            paddingBottom: 10,
-            paddingInlineStart: 10,
-          }}
-        >
-          <Nav
-            className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: "550px" }}
-            navbarScroll
-          >
-            <Nav.Link
-              href="#action1"
-              style={{ color: "black", fontSize: 14, fontWeight: "bold" }}
+        <S.Collapse>
+          <BSNavbar.Collapse id="navbarScroll">
+            <Nav
+              className="me-auto my-2 my-lg-0"
+              style={{ maxHeight: "550px" }}
+              navbarScroll
             >
-              Inicio
-            </Nav.Link>
-            <Nav.Link
-              href="#action2"
-              style={{ color: "black", fontSize: 14, fontWeight: "bold" }}
-            >
-              ¿Cómo funciona?
-            </Nav.Link>
-            <Nav.Link
-              href="#"
-              style={{ color: "black", fontSize: 14, fontWeight: "bold" }}
-            >
-              Negocios
-            </Nav.Link>
-            <Nav.Link
-              href="#"
-              style={{ color: "black", fontSize: 14, fontWeight: "bold" }}
-            >
-              Contactos
-            </Nav.Link>
-          </Nav>
-          <button
-            className="btn btn-w-m btn-success btn-sm"
-            style={{
-              borderRadius: 10,
-              backgroundColor: "#4237AA",
-              borderColor: "#4237AA",
-              height: 45,
-              width: 130,
-              fontSize: 14,
-              fontWeight: "bold",
-            }}
-          >
-            Iniciar Sesión
-          </button>
-          <button
-            className="btn btn-w-m btn-light btn-sm"
-            style={{
-              marginLeft: 10,
-              borderRadius: 10,
-              borderColor: "#4237AA",
-              height: 45,
-              width: 130,
-              fontSize: 14,
-              fontWeight: "bold",
-              color: "#4237AA",
-              borderStyle: "solid",
-              borderWidth: 2,
-            }}
-          >
-            Regístrate
-          </button>
-        </BSNavbar.Collapse>
+              <Nav.Link href="#action1">
+                <h6>Inicio</h6>
+              </Nav.Link>
+              <Nav.Link href="#action2">
+                <h6>¿Cómo funciona?</h6>
+              </Nav.Link>
+              <Nav.Link href="#">
+                <h6>Negocios</h6>
+              </Nav.Link>
+              <Nav.Link href="#">
+                <h6>Contactos</h6>
+              </Nav.Link>
+            </Nav>
+            <S.BtnSuccess className="btn btn-w-m btn-sm">
+              Iniciar Sesión
+            </S.BtnSuccess>
+            <S.BtnLight className="btn btn-w-m btn-sm">Regístrate</S.BtnLight>
+          </BSNavbar.Collapse>
+        </S.Collapse>
       </Container>
     </S.Navbar>
   );
