b03d7b9eb7663bbc9ef21270dcd34925a8ff2378
Francisco <francisco.ulloa@nextisolutions.com>
2022-11-02 16:29:33 -0500

    [FIX] separe layout into component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,10 @@
-import Navbar from "../components/Navbar";
-import styled from "styled-components";
 import Banner from "../components/Banner";
-import Footer from "../components/Footer";
-
-const Container = styled.div`
-  margin-top: 90px;
-`;
+import Layout from "../components/Layout";
 
 export default function Home() {
   return (
-    <div className="p-0">
-      <Navbar />
-      <Container>
-        <Banner />
-      </Container>
-      <Footer />
-    </div>
+    <Layout>
+      <Banner />
+    </Layout>
   );
 }
