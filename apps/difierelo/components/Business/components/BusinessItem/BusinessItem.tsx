6290aa034cd484b932e976afd51a35bf5b09dc95
Francisco <francisco.ulloa@nextisolutions.com>
2022-11-06 23:20:43 -0500

    [IMP] component

--- apps/difierelo/components/Business/components/BusinessItem/BusinessItem.tsx
+++ apps/difierelo/components/Business/components/BusinessItem/BusinessItem.tsx
@@ -1,11 +1,12 @@
-import * as S from "./BusinessItem.styles";
+import * as S from './BusinessItem.styles';
+import Props from './BusinessItem.props';
 
-const BusinessItem: React.FunctionComponent = () => {
+const BusinessItem: React.FunctionComponent<Props> = ({ name }) => {
   return (
     <S.Card>
       <S.CardImg variant="top" src="/img/Rectangle 24.png" />
       <S.CardBody>
-        <h5>Frecuento</h5>
+        <h5>{name}</h5>
         <p>4 meses sin intereses en tecnología</p>
       </S.CardBody>
     </S.Card>
