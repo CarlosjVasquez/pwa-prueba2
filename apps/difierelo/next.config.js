5cfa2c104c19daf4adc76234ef7f7264b970c0dd
USS Enterprise <ussenterprise@USSs-MacBook-Pro.local>
2022-11-07 19:56:24 -0500

    cambio de output proyecto

--- apps/difierelo/next.config.js
+++ apps/difierelo/next.config.js
@@ -3,27 +3,27 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { withNx } = require('@nrwl/next/plugins/with-nx');
 const withPWA = require('next-pwa')({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development',
+    dest: 'public',
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === 'development',
 });
 
 /**
  * @type {import('@nrwl/next/plugins/with-nx').WithNxOptions}
  **/
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  compiler: {
-    styledComponents: true,
-  },
-  output: 'standalone',
-  nx: {
-    // Set this to true if you would like to to use SVGR
-    // See: https://github.com/gregberge/svgr
-    svgr: false,
-  },
+    reactStrictMode: true,
+    swcMinify: true,
+    compiler: {
+        styledComponents: true,
+    },
+    //output: 'standalone',
+    nx: {
+        // Set this to true if you would like to to use SVGR
+        // See: https://github.com/gregberge/svgr
+        svgr: false,
+    },
 };
 
 module.exports = withPWA(withNx(nextConfig));
