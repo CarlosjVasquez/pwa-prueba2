0a52a364d487feae71e62240fdd3d642023dd1de
Jhon Andrade <jhon.cone.8@gmail.com>
2023-01-17 18:27:44 -0500

    [FIX] Solo numeros

--- apps/difierelo/components/OnboardingOtp/OnboardingOtp.styles.tsx
+++ apps/difierelo/components/OnboardingOtp/OnboardingOtp.styles.tsx
@@ -192,8 +192,8 @@ export const OtpInput = styled.input`
     -moz-appearance: textfield;
   }
 
-  input::-webkit-outer-spin-button,
-  input::-webkit-inner-spin-button {
+  input[type='number']::-webkit-inner-spin-button,
+  input[type='number']::-webkit-outer-spin-button {
     -webkit-appearance: none;
   }
 
